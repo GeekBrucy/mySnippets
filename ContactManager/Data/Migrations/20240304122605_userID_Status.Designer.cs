b089021973cd074aa3b36a485ec280d8ec907b32
GeekBrucy <wanyanlulu@gmail.com>
2024-03-04 23:27:20 +1100

    Added new fields to Contact
    * ran add migration
    * ran migration

--- ContactManager/Data/Migrations/20240304122605_userID_Status.Designer.cs
+++ ContactManager/Data/Migrations/20240304122605_userID_Status.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ContactManager.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ContactManager.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240304122605_userID_Status")]
+    partial class userID_Status
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.16");
--- ContactManager/Models/Contact.cs
+++ ContactManager/Models/Contact.cs
@@ -9,6 +9,8 @@ namespace ContactManager.Models;
 public class Contact
 {
   public int ContactId { get; set; }
+  // user ID from AspNetUser table
+  public string? OwnerId { get; set; }
   public string? Name { get; set; }
   public string? Address { get; set; }
   public string? City { get; set; }
@@ -16,4 +18,13 @@ public class Contact
   public string? Zip { get; set; }
   [DataType(DataType.EmailAddress)]
   public string? Email { get; set; }
+
+  public ContactStatus Status { get; set; }
+}
+
+public enum ContactStatus
+{
+  Submitted,
+  Approved,
+  Rejected
 }
