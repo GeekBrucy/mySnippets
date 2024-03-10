5677131db794e645371f11fa61791b5dd25b0a09
GeekBrucy <wanyanlulu@gmail.com>
2024-03-11 01:30:50 +1100

    added linked data component and material

--- linked-data/src/app/app.module.ts
+++ linked-data/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LinkedDataComponent } from './linked-data/linked-data.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LinkedDataComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ linked-data/src/app/linked-data/linked-data.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-linked-data',
+  templateUrl: './linked-data.component.html',
+  styleUrl: './linked-data.component.scss'
+})
+export class LinkedDataComponent {
+
+}
