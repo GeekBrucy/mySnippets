bb3f6d13e8e294d4699b53daecdba2df01d15a2e
GeekBrucy <wanyanlulu@gmail.com>
2024-03-11 01:54:35 +1100

    Basic setup

--- linked-data/src/app/linked-data/linked-data.component.ts
+++ linked-data/src/app/linked-data/linked-data.component.ts
@@ -6,12 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './linked-data.component.scss'
 })
 export class LinkedDataComponent implements OnInit {
+  public data = [
+    {
+      current: 1,
+      next: 2,
+      desc: "Node 1"
+    },
+    {
+      current: 3,
+      next: 4,
+      desc: "Node 3"
+    },
+    {
+      current: 5,
+      next: null,
+      desc: "Node 5"
+    },
+    {
+      current: 2,
+      next: 3,
+      desc: "Node 2"
+    },
+    {
+      current: 4,
+      next: 5,
+      desc: "Node 4"
+    }
+  ];
+  public sortedData: any[] = [];
   constructor() {
   }
 
-  ngOnInit(): void
-  {
-    console.log("init")
+  ngOnInit(): void {
+    this.sortedData = this.data;
   }
 
 }
