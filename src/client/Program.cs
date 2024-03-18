b2e2109e45ed083a58cd5d49aa27c58a0174be6a
GeekBrucy <wanyanlulu@gmail.com>
2024-03-18 23:31:03 +1100

    Init proj and added wrong usage example

---
+++ src/client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+internal class Program
+{
+  private static async Task Main(string[] args)
+  {
+    await WrongUsage();
+  }
+
+  static async Task WrongUsage()
+  {
+    string fileName = @"./1.txt";
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < 10000; i++)
+    {
+      sb.AppendLine("hello");
+    }
+
+    // 1. note here there is no await
+    File.WriteAllTextAsync(fileName, sb.ToString());
+
+    // 2. Here will throw exception: The process cannot access the file.....
+    // because the process is still writing texts to the target file
+    string s = File.ReadAllText(fileName);
+    Console.WriteLine(s);
+  }
+}
