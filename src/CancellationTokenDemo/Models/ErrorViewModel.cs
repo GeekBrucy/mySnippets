2ee98a4832ae09c82b9df9b874e7b3e9bf5326ce
GeekBrucy <wanyanlulu@gmail.com>
2024-03-20 23:18:43 +1100

    Init MVC proj

---
+++ src/CancellationTokenDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CancellationTokenDemo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
