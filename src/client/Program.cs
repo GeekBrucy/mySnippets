afa462e1e03ce7a96db564c26869559f98ca5eb1
GeekBrucy <wanyanlulu@gmail.com>
2024-03-21 23:39:06 +1100

    lambda demo

--- src/client/Program.cs
+++ src/client/Program.cs
@@ -1,11 +1,14 @@
 ﻿using client.Delegates;
+using client.Lambda;
 
 internal class Program
 {
   private static void Main(string[] args)
   {
-    DelegateDemo.Run();
-    ActionDemo.Run();
-    FuncDemo.Run();
+    // DelegateDemo.Run();
+    // ActionDemo.Run();
+    // FuncDemo.Run();
+
+    LambdaDemo.Run();
   }
 }
