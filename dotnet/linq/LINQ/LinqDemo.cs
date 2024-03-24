cae3a4f321b9a41d2966a54de861aabcc557cb9f
GeekBrucy <wanyanlulu@gmail.com>
2024-03-24 22:34:43 +1100

    Demo Single, First, Order, Skip, Take

--- dotnet/linq/LINQ/LinqDemo.cs
+++ dotnet/linq/LINQ/LinqDemo.cs
@@ -11,13 +11,27 @@ public static class LinqDemo
 {
   public static void Run()
   {
-    var testData = new EmployeeFaker().Generate(100);
+    var testData = new EmployeeFaker().Generate(10);
 
     // DemoWhere(testData);
     // DemoCount(testData);
     // DemoAny(testData);
     // DemoSingle(testData);
-    DemoSingleOrDefault(testData);
+    // DemoSingleOrDefault(testData);
+    // DemoFirst(testData);
+    // DemoFirstOrDefault(testData);
+    // DemoOrderBy(testData);
+    // DemoOrderByDescending(testData);
+    // DemoMultipleOrder(testData);
+    DemoSkipTake(testData);
+  }
+
+  private static void PrintList(IEnumerable<Employee> testData)
+  {
+    foreach (var item in testData)
+    {
+      Console.WriteLine(item);
+    }
   }
 
   static void DemoWhere(IEnumerable<Employee> testData)
@@ -67,5 +81,54 @@ public static class LinqDemo
       // if there are more than 1 records, it will throw exception
       Console.WriteLine("More than 1 records returned!!!");
     }
+  }
+
+  static void DemoFirst(IEnumerable<Employee> testData)
+  {
+    try
+    {
+      Console.WriteLine(testData.First(e => e.Age > 300));
+    }
+    catch (Exception e)
+    {
+      // if there is no match, it will throw exception
+      Console.WriteLine("Not found");
+    }
+  }
+  static void DemoFirstOrDefault(IEnumerable<Employee> testData)
+  {
+    Console.WriteLine(testData.FirstOrDefault(e => e.Age > 300));
+  }
+  static void DemoOrderBy(IEnumerable<Employee> testData)
+  {
+    foreach (var item in testData.OrderBy(e => e.Age))
+    {
+      Console.WriteLine(item);
+    }
+
+    var nums = new int[] { 3, 9, 6, 5, 10, 7 };
+    foreach (var item in nums.OrderBy(i => i))
+    {
+      Console.WriteLine(item);
+    }
+  }
+
+  static void DemoOrderByDescending(IEnumerable<Employee> testData)
+  {
+    foreach (var item in testData.OrderByDescending(e => e.Age))
+    {
+      Console.WriteLine(item);
+    }
+  }
+  static void DemoMultipleOrder(IEnumerable<Employee> testData)
+  {
+    var ordered = testData.OrderBy(e => e.Age).ThenBy(e => e.Salary);
+    PrintList(ordered);
+  }
+
+  static void DemoSkipTake(IEnumerable<Employee> testData)
+  {
+    var data = testData.Skip(3).Take(2);
+    PrintList(data);
   }
 }
