a9720e10194456b25547c184311e17f89cc5ab2a
GeekBrucy <wanyanlulu@gmail.com>
2024-03-29 16:21:02 +1100

    Connected Mail Service with ILog and IConfig
    * Added Mail service to the console client and added DI package

--- dotnet/DI-Integration-Create-Wheel/ConsoleMailClient/Program.cs
+++ dotnet/DI-Integration-Create-Wheel/ConsoleMailClient/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConfigServices.Interfaces;
+using ConfigServices.Services;
+using LogServices.Interfaces;
+using LogServices.Providers;
+using MailServices.Interfaces;
+using MailServices.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+internal class Program
+{
+  private static void Main(string[] args)
+  {
+    ServiceCollection services = new ServiceCollection();
+    services.AddScoped<IConfigService, EnvVarConfigService>();
+    services.AddScoped<IMailService, MailService>();
+    services.AddScoped<ILogProvider, ConsoleLogProvider>();
+    using var sp = services.BuildServiceProvider();
+    var mailService = sp.GetRequiredService<IMailService>();
+    mailService.Send("Test", "test@test.gov", "test mail service");
+  }
+}
--- dotnet/DI-Integration-Create-Wheel/MailServices/Services/MailService.cs
+++ dotnet/DI-Integration-Create-Wheel/MailServices/Services/MailService.cs
@@ -2,14 +2,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ConfigServices.Interfaces;
+using LogServices.Interfaces;
 using MailServices.Interfaces;
 
 namespace MailServices.Services;
 
 public class MailService : IMailService
 {
+  private readonly ILogProvider _logProvider;
+  private readonly IConfigService _config;
+
+  public MailService(ILogProvider logProvider, IConfigService config)
+  {
+    _logProvider = logProvider;
+    _config = config;
+  }
   public void Send(string title, string to, string body)
   {
+    _logProvider.LogInfo("Ready to send email");
+    string smtpServer = _config.GetValue("SmtpServer");
+    string userName = _config.GetValue("UserName");
+    string password = _config.GetValue("Password");
+    Console.WriteLine($"SmtpServer Details: smtpServer = {smtpServer}, username = {userName}, password = {password}");
     Console.WriteLine($"Sent Email! title = {title}, to = {to}, body = {body}");
+    _logProvider.LogInfo("Email Sent");
   }
 }
