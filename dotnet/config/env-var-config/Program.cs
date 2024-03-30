4dc69eba794d2ca20cf4e89749b00804530ecdc0
GeekBrucy <peichunzhou1006@gmail.com>
2024-03-30 16:50:16 +1100

    basic env var config usage

--- dotnet/config/env-var-config/Program.cs
+++ dotnet/config/env-var-config/Program.cs
@@ -1,7 +1,15 @@
-﻿internal class Program
+﻿using Microsoft.Extensions.Configuration;
+
+internal class Program
 {
   private static void Main(string[] args)
   {
+    ConfigurationBuilder configBuilder = new ConfigurationBuilder();
 
+    configBuilder.AddEnvironmentVariables();
+    IConfigurationRoot configRoot = configBuilder.Build();
+
+    var envVar = configRoot.GetSection("Path").Value;
+    Console.WriteLine(envVar);
   }
 }
