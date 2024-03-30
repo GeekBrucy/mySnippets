799a801b7001ecb306d95fdf6bbc2366a68b5bc7
GeekBrucy <peichunzhou1006@gmail.com>
2024-03-30 15:03:20 +1100

    Basic usage of configuration

--- dotnet/config/json-config/Program.cs
+++ dotnet/config/json-config/Program.cs
@@ -1,7 +1,21 @@
-﻿internal class Program
+﻿using Microsoft.Extensions.Configuration;
+
+internal class Program
 {
   private static void Main(string[] args)
   {
-    Console.WriteLine("Hello, World!");
+    ConfigurationBuilder configBuilder = new ConfigurationBuilder();
+    configBuilder.AddJsonFile
+    (
+      "config.json",
+      optional: true,
+      reloadOnChange: true
+    );
+    IConfigurationRoot configRoot = configBuilder.Build();
+
+    Console.WriteLine(configRoot["name"]);
+    Console.WriteLine(configRoot["proxy:address"]);
+    string address = configRoot.GetSection("proxy:address").Value;
+    Console.WriteLine(address);
   }
 }
