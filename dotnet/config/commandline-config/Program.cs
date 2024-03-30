d12b07a9de24d83db6128f7a22e348fd642ad415
GeekBrucy <peichunzhou1006@gmail.com>
2024-03-30 16:43:23 +1100

    basic usage of command line config

--- dotnet/config/commandline-config/Program.cs
+++ dotnet/config/commandline-config/Program.cs
@@ -1,7 +1,17 @@
-﻿internal class Program
+﻿using Microsoft.Extensions.Configuration;
+
+internal class Program
 {
   private static void Main(string[] args)
   {
-    Console.WriteLine("Hello, World!");
+    ConfigurationBuilder configBuilder = new ConfigurationBuilder();
+
+    configBuilder.AddCommandLine(args);
+    IConfigurationRoot configRoot = configBuilder.Build();
+    // when `dotnet run`, just add `age=100`
+    // so the full command will be `dotnet run age=100`
+    // do similar thing when running the executable file
+    var age = configRoot.GetSection("age").Value;
+    Console.WriteLine(age);
   }
 }
