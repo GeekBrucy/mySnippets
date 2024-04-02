5e28025cd8467cfdbf7c08fd9693249609b653c5
GeekBrucy <peichunzhou1006@gmail.com>
2024-04-02 22:57:12 +1100

    Added EFClient for demo
    * Added basic save

---
+++ dotnet/Entity-Framework-Core/EFClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using EFConfigs.Data;
+using EFConfigs.Models;
+
+internal class Program
+{
+  private static async Task Main(string[] args)
+  {
+    using MyDbContext ctx = new MyDbContext();
+
+    Person p = new Person
+    {
+      Age = 18,
+      Name = "Test",
+      BirthPlace = "Somewhere",
+      Salary = 13
+    };
+
+    ctx.Persons.Add(p);
+    await ctx.SaveChangesAsync();
+    Console.WriteLine("Saved");
+  }
+}
