e82d65852f4ec3f6a06e60e212c08c47a8b5a454
GeekBrucy <peichunzhou1006@gmail.com>
2024-04-03 22:11:26 +1100

    Demoed CRUD in EF core

--- dotnet/Entity-Framework-Core/EFClient/Program.cs
+++ dotnet/Entity-Framework-Core/EFClient/Program.cs
@@ -6,17 +6,49 @@ internal class Program
   private static async Task Main(string[] args)
   {
     using MyDbContext ctx = new MyDbContext();
-
-    Person p = new Person
-    {
-      Age = 18,
-      Name = "Test",
-      BirthPlace = "Somewhere",
-      Salary = 13
-    };
-
-    ctx.Persons.Add(p);
+    // await DemoSave(ctx);
+    // DemoQuery(ctx);
+    // await DemoUpdate(ctx);
+    await DemoDelete(ctx);
+  }
+  static async Task DemoSave(MyDbContext ctx)
+  {
+    int bookAmount = 5;
+    var books = new BookFaker().Generate(bookAmount);
+    ctx.AddRange(books);
     await ctx.SaveChangesAsync();
-    Console.WriteLine("Saved");
+    Console.WriteLine($"Created {bookAmount} books");
+  }
+
+  static void DemoQuery(MyDbContext ctx)
+  {
+    IQueryable<Book> filteredBooks = ctx.Books.Where(b => b.Price > 40);
+    Console.WriteLine($"{filteredBooks.Count()} book(s) Price(s) is/are greater than 40");
+
+    var books = ctx.Books.GroupBy(b => b.Author)
+      .Select(g => new { Name = g.Key, BooksCount = g.Count(), MaxPrice = g.Max(b => b.Price) });
+    foreach (var book in books)
+    {
+      Console.WriteLine($"{book.Name}, Total={book.BooksCount}, MaxPrice={book.MaxPrice}");
+    }
+  }
+
+  static async Task DemoUpdate(MyDbContext ctx)
+  {
+    var b = ctx.Books.First(b => b.Id == 1);
+
+    b.Title = "Dotnet EF Core Update";
+    await ctx.SaveChangesAsync();
+    Console.WriteLine("Updated");
+  }
+
+  static async Task DemoDelete(MyDbContext ctx)
+  {
+    var b = ctx.Books.First(b => b.Id == 3);
+    ctx.Books.Remove(b);
+
+    await ctx.SaveChangesAsync();
+
+    Console.WriteLine("Deleted");
   }
 }
