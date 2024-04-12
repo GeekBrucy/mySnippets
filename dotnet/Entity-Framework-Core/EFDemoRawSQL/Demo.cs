7e485c83701a6856d1a62d183ff5cc5a353d4f6e
GeekBrucy <peichunzhou1006@gmail.com>
2024-04-12 22:54:38 +1000

    Demoed using ado.net in ef core

--- dotnet/Entity-Framework-Core/EFDemoRawSQL/Demo.cs
+++ dotnet/Entity-Framework-Core/EFDemoRawSQL/Demo.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Threading.Tasks;
 using EFConfigs.Models;
@@ -14,7 +15,8 @@ public class Demo : BaseDemo
   public override async Task RunAsync()
   {
     // await DemoInsert();
-    await DemoRawQueryWithEntity();
+    // await DemoRawQueryWithEntity();
+    // await DemoAdoNet();
   }
   private async Task DemoInsert()
   {
@@ -39,6 +41,26 @@ public class Demo : BaseDemo
     await foreach (var a in articles.AsAsyncEnumerable())
     {
       Console.WriteLine(a);
+    }
+  }
+
+  private async Task DemoAdoNet()
+  {
+    DbConnection conn = _ctx.Database.GetDbConnection();
+    if (conn.State != System.Data.ConnectionState.Open)
+    {
+      await conn.OpenAsync();
+    }
+
+    using var cmd = conn.CreateCommand();
+    cmd.CommandText = "select * from articles order by rand() limit 10";
+    using var reader = cmd.ExecuteReader();
+    while (await reader.ReadAsync())
+    {
+      long id = reader.GetInt64(0);
+      string title = reader.GetString(1);
+      string content = reader.GetString(2);
+      Console.WriteLine($"id={id}, title={title}, content={content}");
     }
   }
 }
