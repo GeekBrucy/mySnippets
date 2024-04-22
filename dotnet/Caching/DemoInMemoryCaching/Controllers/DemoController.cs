4f2ce2b0064f1abb9df9ede59b360ab3575f6859
GeekBrucy <peichunzhou1006@gmail.com>
2024-04-22 21:32:42 +1000

    added demo controller

---
+++ dotnet/Caching/DemoInMemoryCaching/Controllers/DemoController.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoInMemoryCaching.Controllers;
+
+[ApiController]
+[Route("api/[controller]/[action]")]
+public class DemoController : ControllerBase
+{
+}
