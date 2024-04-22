8f059a39720efcbdbfebed2fc8e488f56d4b0ae2
GeekBrucy <peichunzhou1006@gmail.com>
2024-04-22 21:47:43 +1000

    register memory cache service and basic usage

--- dotnet/Caching/DemoInMemoryCaching/Controllers/DemoController.cs
+++ dotnet/Caching/DemoInMemoryCaching/Controllers/DemoController.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Data;
+using DemoResponseCache.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace DemoInMemoryCaching.Controllers;
 
@@ -10,4 +13,23 @@ namespace DemoInMemoryCaching.Controllers;
 [Route("api/[controller]/[action]")]
 public class DemoController : ControllerBase
 {
+  private readonly IMemoryCache _memoryCache;
+
+  public DemoController(IMemoryCache memoryCache)
+  {
+    _memoryCache = memoryCache;
+  }
+
+  [HttpGet]
+  public ActionResult<Book?> GetBookById(long id)
+  {
+    Book? result = FakeDbContext.GetById(id);
+
+    if (result == null)
+    {
+      return NotFound($"Cannot find book with id {id}");
+    }
+
+    return result;
+  }
 }
