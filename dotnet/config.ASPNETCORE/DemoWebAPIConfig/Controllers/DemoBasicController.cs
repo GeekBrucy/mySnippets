ee7f283c2d8f312e0342e5ece61aed5713fb69c5
GeekBrucy <peichunzhou1006@gmail.com>
2024-04-25 23:11:36 +1000

    demoed get environment variable

--- dotnet/config.ASPNETCORE/DemoWebAPIConfig/Controllers/DemoBasicController.cs
+++ dotnet/config.ASPNETCORE/DemoWebAPIConfig/Controllers/DemoBasicController.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -7,8 +9,23 @@ using Microsoft.AspNetCore.Mvc;
 namespace DemoWebAPIConfig.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/[controller]/[action]")]
 public class DemoBasicController : ControllerBase
 {
-    
+  [HttpGet]
+  public IDictionary<string, string> GetSystemEnvVar()
+  {
+    IDictionary<string, string> envVar = new Dictionary<string, string>();
+    foreach (DictionaryEntry item in Environment.GetEnvironmentVariables())
+    {
+      envVar.Add(item.Key.ToString(), item.Value.ToString());
+    }
+    return envVar;
+  }
+  [HttpGet]
+  public string GetEnvVar()
+  {
+    var val = Environment.GetEnvironmentVariable("TEST");
+    return val;
+  }
 }
