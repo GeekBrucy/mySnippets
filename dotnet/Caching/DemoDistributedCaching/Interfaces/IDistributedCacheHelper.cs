674841cb178c764ab9f61c709e890d9e458ea0cd
GeekBrucy <peichunzhou1006@gmail.com>
2024-04-25 21:48:22 +1000

    added distributed cache helper
    * source: https://github.com/yangzhongke/NETBookMaterials/blob/main/%E7%AC%AC%E4%B8%83%E7%AB%A0/%E5%88%86%E5%B8%83%E5%BC%8F%E7%BC%93%E5%AD%98/DistributedCacheHelper.cs

---
+++ dotnet/Caching/DemoDistributedCaching/Interfaces/IDistributedCacheHelper.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace DemoDistributedCaching.Interfaces;
+
+public interface IDistributedCacheHelper
+{
+  TResult? GetOrCreate<TResult>(string cacheKey, Func<DistributedCacheEntryOptions, TResult?> valueFactory, int expireSeconds);
+  Task<TResult?> GetOrCreateAsync<TResult>(string cacheKey, Func<DistributedCacheEntryOptions, Task<TResult?>> valueFactory, int expireSeconds);
+  void Remove(string cacheKey);
+  Task RemoveAsync(string cacheKey);
+}
