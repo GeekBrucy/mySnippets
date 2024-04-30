4e6bc7e050c0d7da5364759879e2047a42e0ba02
GeekBrucy <peichunzhou1006@gmail.com>
2024-04-30 22:04:45 +1000

    demoed simple usage of middleware

--- dotnet/Middleware/MiddlewareIntro/Program.cs
+++ dotnet/Middleware/MiddlewareIntro/Program.cs
@@ -3,4 +3,25 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.Map("/test", async (pipeBuilder) =>
+{
+  pipeBuilder.Use(async (context, next) =>
+  {
+    context.Response.ContentType = "text/html";
+    await context.Response.WriteAsync("1 start <br/>");
+    await next.Invoke(); // invoke line 17
+    await context.Response.WriteAsync("1 end <br/>");
+  });
+  pipeBuilder.Use(async (context, next) =>
+  {
+    await context.Response.WriteAsync("2 start <br/>");
+    await next.Invoke(); // invoke line 23
+    await context.Response.WriteAsync("2 end <br/>");
+  });
+  pipeBuilder.Run(async ctx =>
+  {
+    await ctx.Response.WriteAsync("Run <br/>");
+  });
+});
+
 app.Run();
