cb54dc0b0a7049de51309cb9c5f498703a362de6
GeekBrucy <peichunzhou1006@gmail.com>
2024-05-01 23:29:11 +1000

    configured identity server and ef core

--- dotnet/auth/IdentityServer/01_IdentityServerBasic/Program.cs
+++ dotnet/auth/IdentityServer/01_IdentityServerBasic/Program.cs
@@ -1,3 +1,8 @@
+using _01_IdentityServerBasic.Data;
+using _01_IdentityServerBasic.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +11,28 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<MyDbContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("postgres"));
+});
+builder.Services.AddDataProtection();
+// do not use AddIdentity in webapi, because that's for MVC
+builder.Services.AddIdentityCore<CustomUser>(opt =>
+{
+    // the following rules are for testing purpose
+    opt.Password.RequireDigit = false;
+    opt.Password.RequireLowercase = false;
+    opt.Password.RequireNonAlphanumeric = false;
+    opt.Password.RequireUppercase = false;
+    opt.Password.RequiredLength = 6;
+    opt.Tokens.PasswordResetTokenProvider = TokenOptions.DefaultEmailProvider;
+    opt.Tokens.EmailConfirmationTokenProvider = TokenOptions.DefaultEmailProvider;
+});
+var idBuilder = new IdentityBuilder(typeof(CustomUser), typeof(CustomRole), builder.Services);
+idBuilder.AddEntityFrameworkStores<MyDbContext>()
+    .AddDefaultTokenProviders()
+    .AddRoleManager<RoleManager<CustomRole>>()
+    .AddUserManager<UserManager<CustomUser>>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
