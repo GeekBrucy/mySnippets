dc5129cc1492c7bc70464dff6c4b4087d4604bbc
GeekBrucy <peichunzhou1006@gmail.com>
2024-05-02 22:50:51 +1000

    demoed create role, user, assign role to user, and check login

--- dotnet/auth/IdentityServer/01_IdentityServerBasic/Controllers/DemoController.cs
+++ dotnet/auth/IdentityServer/01_IdentityServerBasic/Controllers/DemoController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using _01_IdentityServerBasic.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace _01_IdentityServerBasic.Controllers;
@@ -10,5 +12,108 @@ namespace _01_IdentityServerBasic.Controllers;
 [Route("api/[controller]/[action]")]
 public class DemoController : ControllerBase
 {
+  private readonly UserManager<CustomUser> _userManager;
+  private readonly RoleManager<CustomRole> _roleManager;
+  public DemoController(UserManager<CustomUser> userManager, RoleManager<CustomRole> roleManager)
+  {
+    _userManager = userManager;
+    _roleManager = roleManager;
+  }
 
+  [HttpPost]
+  public async Task<ActionResult<IdentityResult>> CreateRole(string roleName)
+  {
+    if (!await _roleManager.RoleExistsAsync(roleName))
+    {
+      CustomRole newRole = new CustomRole { Name = roleName };
+      Console.WriteLine("creating role");
+      var result = await _roleManager.CreateAsync(newRole);
+      if (result.Succeeded == false)
+      {
+        return BadRequest(result.Errors);
+      }
+      else
+      {
+        return Ok("created role");
+      }
+    }
+
+    return IdentityResult.Success;
+  }
+
+  [HttpPost]
+  public async Task<ActionResult<IdentityResult>> CreateUser(string userName)
+  {
+    CustomUser customUser = await _userManager.FindByNameAsync(userName);
+    if (customUser == null)
+    {
+      customUser = new CustomUser { UserName = userName };
+      var result = await _userManager.CreateAsync(customUser, "123456");
+      if (result.Succeeded == false)
+      {
+        return BadRequest(result.Errors);
+      }
+      else
+      {
+        return Ok("created user");
+      }
+    }
+    return BadRequest("user exists");
+  }
+
+  [HttpPost]
+  public async Task<ActionResult<IdentityResult>> AssignUserRole(string userName, string roleName)
+  {
+    if (!await _roleManager.RoleExistsAsync(roleName))
+    {
+      return NotFound($"{roleName} role not found");
+    }
+    CustomUser user = await _userManager.FindByNameAsync(userName);
+    if (user == null)
+    {
+      return NotFound($"No user with name '{userName}'");
+    }
+
+    if (await _userManager.IsInRoleAsync(user, roleName))
+    {
+      return BadRequest($"{userName} already has {roleName} role");
+    }
+
+    var result = await _userManager.AddToRoleAsync(user, roleName);
+    if (result.Succeeded == false)
+    {
+      return BadRequest(result.Errors);
+    }
+    else
+    {
+      return Ok("role assigned");
+    }
+  }
+
+  [HttpPost]
+  public async Task<ActionResult<IdentityResult>> CheckUserLogin(string userName, string password)
+  {
+    CustomUser user = await _userManager.FindByNameAsync(userName);
+    if (user == null)
+    {
+      return NotFound($"No user with name '{userName}'"); // not found will introduce security issue, do not use in production
+    }
+
+    if (await _userManager.IsLockedOutAsync(user))
+    {
+      return BadRequest("User locked");
+    }
+
+    if (await _userManager.CheckPasswordAsync(user, password))
+    {
+      await _userManager.ResetAccessFailedCountAsync(user); // reset failed login count
+      return Ok("Login successfully");
+    }
+    else
+    {
+      await _userManager.AccessFailedAsync(user); // record failed login count
+      return BadRequest("Invalid operation");
+    }
+
+  }
 }
