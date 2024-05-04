973bf4aec8e3975311cd85830fb0cff633c96d4a
GeekBrucy <peichunzhou1006@gmail.com>
2024-05-04 23:03:59 +1000

    init demo controller

---
+++ dotnet/auth/IdentityServer/02_JWT/Controllers/DemoController.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _02_JWT.Controllers;
+
+[ApiController]
+[Route("api/[controller]/[action]")]
+public class DemoController : ControllerBase
+{
+
+}
