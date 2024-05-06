76f2a15df9455b8b47332c47aa64d657ec05b45e
GeekBrucy <peichunzhou1006@gmail.com>
2024-05-06 22:20:49 +1000

    allow header input in swagger

--- dotnet/auth/IdentityServer/02_JWT/Program.cs
+++ dotnet/auth/IdentityServer/02_JWT/Program.cs
@@ -2,6 +2,7 @@ using System.Reflection;
 using IdentityServerConfig.Extensions;
 using IdentityServerConfig.Helpers;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +11,22 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    var scheme = new OpenApiSecurityScheme()
+    {
+        Description = "Authorization header. \r\nExample: 'Bearer 12345abcdef'",
+        Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Authorization" },
+        Scheme = "oauth2",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey
+    };
+    c.AddSecurityDefinition("Authorization", scheme);
+    var requirement = new OpenApiSecurityRequirement();
+    requirement[scheme] = new List<string>();
+    c.AddSecurityRequirement(requirement);
+});
 Action<DbContextOptionsBuilder> dbContextBuilder = opt =>
 {
     var connStr = builder.Configuration.GetConnectionString("postgres");
