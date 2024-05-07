aa0cca4a0fe2490c3b12bd37ec60e960bfd7ae91
GeekBrucy <peichunzhou1006@gmail.com>
2024-05-08 00:04:55 +1000

    added JwtVersion to help revoke jwt

---
+++ dotnet/auth/IdentityServer/02_JWT/Migrations/20240507140104_add_jwtversion_to_user.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _02_JWT.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_jwtversion_to_user : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "JwtVersion",
+                table: "AspNetUsers",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "JwtVersion",
+                table: "AspNetUsers");
+        }
+    }
+}
--- dotnet/auth/IdentityServer/IdentityServerConfig/Models/CustomUser.cs
+++ dotnet/auth/IdentityServer/IdentityServerConfig/Models/CustomUser.cs
@@ -8,5 +8,10 @@ namespace IdentityServerConfig.Models;
 
 public class CustomUser : IdentityUser<long>
 {
+  /*
+    this field is used to handle revoke jwt. If the user login on 2 devices, the later login need to invalid the earlier login 
 
+    or if the user is deleted, and they still use the old jwt to send request, that request needs to be rejected
+  */
+  public long JwtVersion { get; set; }
 }
