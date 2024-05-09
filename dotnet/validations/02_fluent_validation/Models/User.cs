cb51c8cde2a04880e7dc7ae04380dafcf544e8ed
GeekBrucy <peichunzhou1006@gmail.com>
2024-05-09 22:42:53 +1000

    fluent validation sample models

---
+++ dotnet/validations/02_fluent_validation/Models/User.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _02_fluent_validation.Models;
+
+public class User
+{
+  public long Id { get; set; }
+  public string Username { get; set; }
+  public string Email { get; set; }
+  public string Password { get; set; }
+}
