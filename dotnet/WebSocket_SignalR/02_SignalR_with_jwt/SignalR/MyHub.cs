6477fb7e52b9cfde61dfd82305436eb50cd708e6
GeekBrucy <peichunzhou1006@gmail.com>
2024-05-11 22:09:05 +1000

    Added authorize to myhub

--- dotnet/WebSocket_SignalR/02_SignalR_with_jwt/SignalR/MyHub.cs
+++ dotnet/WebSocket_SignalR/02_SignalR_with_jwt/SignalR/MyHub.cs
@@ -1,17 +1,22 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
 namespace _02_SignalR_with_jwt.SignalR;
 
+[Authorize]
 public class MyHub : Hub
 {
   public Task SendPublicMsg(string msg)
   {
+    var claim = Context.User.FindFirst(ClaimTypes.Name);
+
     string connId = Context.ConnectionId;
-    string msgToSend = $"{connId} {DateTime.Now}: {msg}";
+    string msgToSend = $"[{DateTime.Now}] {claim?.Value ?? Context.GetHttpContext().Connection.RemoteIpAddress.ToString()}: {msg} ";
     return Clients.All.SendAsync("PublicMsgReceived", msgToSend);
   }
 }
