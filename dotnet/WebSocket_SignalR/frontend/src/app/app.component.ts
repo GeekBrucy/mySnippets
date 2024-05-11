c53365154922c7084538fb93a33e63bd2cc38e6f
GeekBrucy <peichunzhou1006@gmail.com>
2024-05-11 21:58:04 +1000

    added simple router

--- dotnet/WebSocket_SignalR/frontend/src/app/app.component.ts
+++ dotnet/WebSocket_SignalR/frontend/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { WebsocketMyhubComponent } from './websocket-myhub/websocket-myhub.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, WebsocketMyhubComponent],
+  imports: [CommonModule, RouterOutlet, WebsocketMyhubComponent, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- dotnet/WebSocket_SignalR/frontend/src/app/app.routes.ts
+++ dotnet/WebSocket_SignalR/frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { WebsocketMyhubComponent } from './websocket-myhub/websocket-myhub.component';
+import { WebsocketWithAuthComponent } from './websocket-with-auth/websocket-with-auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "", component: WebsocketMyhubComponent },
+  {path: "websocket-auth", component: WebsocketWithAuthComponent}
+];
