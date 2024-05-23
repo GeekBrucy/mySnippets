2b3463eeef4dbc0ad70664ee1b998401a917fed4
GeekBrucy <peichunzhou1006@gmail.com>
2024-05-23 22:40:15 +1000

    added simple controller to add and fetch flag

--- Azure/AppInsights/DotnetIntegration/01_Basic/Controllers/CatchTheFlagController.cs
+++ Azure/AppInsights/DotnetIntegration/01_Basic/Controllers/CatchTheFlagController.cs
@@ -2,13 +2,43 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using _01_Basic.Data;
+using _01_Basic.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace _01_Basic.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/[controller]/[action]")]
 public class CatchTheFlagController : ControllerBase
 {
+  private readonly MyDbContext _myDbContext;
+  public CatchTheFlagController(MyDbContext myDbContext)
+  {
+    _myDbContext = myDbContext;
+  }
+  [HttpPost]
+  public async Task<ActionResult<Flag>> AddFlag(string value)
+  {
+    var flag = new Flag
+    {
+      Value = value
+    };
 
+    _myDbContext.Flags.Add(flag);
+
+    await _myDbContext.SaveChangesAsync();
+
+    return new OkObjectResult(flag);
+  }
+
+  [HttpGet]
+  public async Task<ActionResult<Flag>> CatchFlag(int? id)
+  {
+    id = id ?? 1;
+
+    var flag = await _myDbContext.Flags.FindAsync(id);
+
+    return new OkObjectResult(flag);
+  }
 }
