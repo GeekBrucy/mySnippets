3d657037262444b9dd7b9ea17d478bf73c7a027d
GeekBrucy <peichunzhou1006@gmail.com>
2024-06-08 23:17:58 +1000

    basic rabbitmq console demo

---
+++ Architecture/DomainDrivenDesign/dotnet_examples/RabbitMQConsoleDemo/Receiver/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var connFactory = new ConnectionFactory();
+var exchangeName = "exchange1";
+string queueName = "queue1";
+string routingKey = "key1";
+connFactory.HostName = "localhost";
+connFactory.DispatchConsumersAsync = true;
+var connection = connFactory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchangeName, "direct");
+channel.QueueDeclare(queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+channel.QueueBind(queue: queueName, exchange: exchangeName, routingKey: routingKey);
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+
+consumer.Received += ConsumerReceived;
+channel.BasicConsume(queueName, false, consumer);
+Console.ReadLine();
+async Task ConsumerReceived(object sender, BasicDeliverEventArgs args)
+{
+  try
+  {
+    var bytes = args.Body.ToArray();
+    string msg = Encoding.UTF8.GetString(bytes);
+    Console.WriteLine($"[{DateTime.Now}] Received Message: {msg}");
+    channel.BasicAck(args.DeliveryTag, multiple: false);
+    await Task.Delay(800);
+  }
+  catch (Exception ex)
+  {
+    channel.BasicReject(args.DeliveryTag, true);
+    Console.WriteLine(ex);
+  }
+
+}
---
+++ Architecture/DomainDrivenDesign/dotnet_examples/RabbitMQConsoleDemo/Sender/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var connFactory = new ConnectionFactory();
+var exchangeName = "exchange1";
+connFactory.HostName = "localhost";
+connFactory.DispatchConsumersAsync = true;
+var connection = connFactory.CreateConnection();
+while (true)
+{
+  using var channel = connection.CreateModel();
+  var props = channel.CreateBasicProperties();
+  props.DeliveryMode = 2; //Non-persistent (1) or persistent (2).
+  channel.ExchangeDeclare(exchangeName, "direct");
+
+  byte[] bytes = Encoding.UTF8.GetBytes(DateTime.Now.ToString());
+  channel.BasicPublish(exchangeName, routingKey: "key1", mandatory: true, basicProperties: props, body: bytes);
+  Thread.Sleep(1000);
+}
