5e7343ac551fffce928a99fdcfc88633b22937d6
GeekBrucy <peichunzhou1006@gmail.com>
2024-06-13 23:22:59 +1000

    added migrations

---
+++ Architecture/DomainDrivenDesign/dotnet_examples/DDD_Example/Users.WebAPI/Migrations/20240613132223_init.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Users.WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserLoginHistories",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: true),
+                    PhoneNumber_RegionCode = table.Column<int>(type: "integer", nullable: false),
+                    PhoneNumber_Number = table.Column<string>(type: "character varying(20)", unicode: false, maxLength: 20, nullable: false),
+                    CreatedDateTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Message = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserLoginHistories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    PhoneNumber_RegionCode = table.Column<int>(type: "integer", nullable: false),
+                    PhoneNumber_Number = table.Column<string>(type: "character varying(20)", unicode: false, maxLength: 20, nullable: false),
+                    passwordHash = table.Column<string>(type: "character varying(100)", unicode: false, maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserAccessFail",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    LockoutEnd = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    AccessFailedCount = table.Column<int>(type: "integer", nullable: false),
+                    isLockOut = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserAccessFail", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserAccessFail_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserAccessFail_UserId",
+                table: "UserAccessFail",
+                column: "UserId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserAccessFail");
+
+            migrationBuilder.DropTable(
+                name: "UserLoginHistories");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
