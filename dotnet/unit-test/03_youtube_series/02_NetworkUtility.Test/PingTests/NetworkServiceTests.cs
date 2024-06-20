63be24696f7605d8b8dc65418d5aa7c89b2c96f1
GeekBrucy <peichunzhou1006@gmail.com>
2024-06-20 22:24:48 +1000

    init 2nd episode - using xunit

---
+++ dotnet/unit-test/03_youtube_series/02_NetworkUtility.Test/PingTests/NetworkServiceTests.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace _02_NetworkUtility.Test.PingTests;
+
+public class NetworkServiceTests
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
