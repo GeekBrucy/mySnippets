5a1825ef6ef1f67d62b7c68948a76e45583407b7
GeekBrucy <peichunzhou1006@gmail.com>
2025-01-06 14:47:38 +1100

    udemy auth course - init code of tutorial 5

---
+++ dotnet/auth/udemy_auth_course/01_v05_webapp_security/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _01_v05_webapp_security.Pages;
+
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
