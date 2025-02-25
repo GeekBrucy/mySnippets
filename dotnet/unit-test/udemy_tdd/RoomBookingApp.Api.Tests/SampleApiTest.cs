5d4d9ad0ee21b5fdfcd0dd8e3736f88e7a8a6f3d
Bruce <peichunzhou1006@gmail.com>
2025-02-25 22:23:45 +1100

    simple demo of webapi controller unit test

---
+++ dotnet/unit-test/udemy_tdd/RoomBookingApp.Api.Tests/SampleApiTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using RoomBookingApp.Api.Controllers;
+using Shouldly;
+
+namespace RoomBookingApp.Api.Tests;
+
+public class SampleApiTest
+{
+    [Fact]
+    public void Should_Return_Forecast_Results()
+    {
+        // Arrange
+        var loggerMock = new Mock<ILogger<WeatherForecastController>>();
+        var controller = new WeatherForecastController(loggerMock.Object);
+
+        // Act
+        var result = controller.Get();
+
+        // Assert
+        result.Count().ShouldBeGreaterThan(1);
+        result.ShouldNotBeNull();
+    }
+}
