ad45ed195aaad57d4c5156870c9dbc65178cf829
Bruce <peichunzhou1006@gmail.com>
2025-03-10 12:08:49 +1100

    updated beanstalk websocket deployment

--- nestJs/websocket-explore/scripts/zip_code.sh
+++ nestJs/websocket-explore/scripts/zip_code.sh
@@ -2,4 +2,4 @@
 
 cd ..
 
-zip -r nestjs-websocket-app.zip . -x "node_modules/*" -x "dist/*" -x ".git/*" -x ".elasticbeanstalk" -x "scripts/*"
+zip -r nestjs-websocket-app.zip . -x "node_modules/*" -x "dist/*" -x ".git/*" -x ".elasticbeanstalk/*" -x "scripts/*"
