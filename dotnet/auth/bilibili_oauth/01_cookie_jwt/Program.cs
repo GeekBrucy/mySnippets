194ce4c963fa9542c61cbb43e7da19f78df670fe
Bruce <peichunzhou1006@gmail.com>
2025-03-16 15:22:29 +1100

    ugly custom auth

--- dotnet/auth/bilibili_oauth/01_cookie_jwt/Program.cs
+++ dotnet/auth/bilibili_oauth/01_cookie_jwt/Program.cs
@@ -5,7 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<MyAuthService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,9 +16,35 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.MapGet("other", (HttpContext ctx) =>
+{
+    var cookie = ctx.Request.Headers.Cookie.FirstOrDefault(x => x.StartsWith("auth="));
 
+    return cookie ?? "empty";
+});
 app.UseAuthorization();
+app.MapGet("login", (MyAuthService auth) =>
+{
+    auth.Signin();
+    return "ok";
+});
 
 app.MapControllers();
 
 app.Run();
+
+
+public class MyAuthService
+{
+    private readonly IHttpContextAccessor _ctx;
+
+    public MyAuthService(IHttpContextAccessor ctx)
+    {
+        _ctx = ctx;
+    }
+
+    public void Signin()
+    {
+        _ctx.HttpContext.Response.Headers["set-cookie"] = "auth=name:test";
+    }
+}
