d58a943a3e10c2b4404da854f0d5f8b757e65281
Bruce <peichunzhou1006@gmail.com>
2025-06-23 21:21:45 +1000

    reset

--- dotnet/auth/certificate_auth/cert_auth_server/Program.cs
+++ dotnet/auth/certificate_auth/cert_auth_server/Program.cs
@@ -1,25 +1,8 @@
-using Microsoft.AspNetCore.Authentication.Certificate;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-
-// 配置证书认证
-builder.Services.AddAuthentication(CertificateAuthenticationDefaults.AuthenticationScheme)
-    .AddCertificate(options =>
-    {
-        options.AllowedCertificateTypes = CertificateTypes.All;
-        options.ValidateCertificateUse = true;
-        options.ValidateValidityPeriod = true;
-        options.RevocationFlag = System.Security.Cryptography.X509Certificates.X509RevocationFlag.EntireChain;
-        options.RevocationMode = System.Security.Cryptography.X509Certificates.X509RevocationMode.NoCheck;
-    });
-
-// 配置授权
-builder.Services.AddAuthorization();
-
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -33,7 +16,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
